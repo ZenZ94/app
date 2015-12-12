e8f2f7f3a536e69f07271d59a1ca2fe7d700598f
ZenZ94 <xxshenghanxx@hotmail.com>
2015-12-12 17:32:37 +0900

    ,k

--- config/models.js
+++ config/models.js
@@ -1,14 +1,3 @@
-/**
- * Default model configuration
- * (sails.config.models)
- *
- * Unless you override them, the following properties will be included
- * in each of your models.
- *
- * For more info on Sails models, see:
- * http://sailsjs.org/#!/documentation/concepts/ORM
- */
-
 module.exports.models = {
 
   /***************************************************************************
@@ -27,6 +16,6 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  // migrate: 'alter'
+  migrate: 'alter'
 
 };
